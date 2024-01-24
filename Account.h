e22fb42a0610d8b405c75c1e4446ed0bb973c553
Jonathan Steele <iNoles@users.noreply.github.com>
2024-01-24 01:27:02 -0500

    apply clang-format

--- Account.h
+++ Account.h
@@ -1,43 +1,41 @@
-#ifndef ACCOUNT_H 
-#define ACCOUNT_H 
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
 #include <iostream>
 
 using namespace std;
 
 class Account {
-    protected:
-    string accountHolderName;
-    double balance;
+protected:
+  string accountHolderName;
+  double balance;
 
-    public:
-    // Constructor
-    Account(const string& holderName, double initialBalance)
-        : accountHolderName(holderName), balance(initialBalance) {}
+public:
+  // Constructor
+  Account(const string &holderName, double initialBalance)
+      : accountHolderName(holderName), balance(initialBalance) {}
 
-    // Member functions
-    virtual void displayAccountInfo() const {
-        cout << "Account Holder: " << accountHolderName << endl;
-        cout << "Balance: $" << balance << endl;
+  // Member functions
+  virtual void displayAccountInfo() const {
+    cout << "Account Holder: " << accountHolderName << endl;
+    cout << "Balance: $" << balance << endl;
+  }
+  virtual void makeDeposit(double amount) {
+    if (amount <= 0) {
+      throw invalid_argument("amount must be postive");
     }
-    virtual void makeDeposit(double amount) {
-        if (amount <= 0) {
-            throw invalid_argument("amount must be postive");
-        }
-        balance += amount;
+    balance += amount;
+  }
+  virtual void makeWithdrawl(double amount) {
+    if (amount <= 0) {
+      throw invalid_argument("amount must be postive");
     }
-    virtual void makeWithdrawl(double amount) {
-        if (amount <= 0) {
-            throw invalid_argument("amount must be postive");
-        }
-        balance -= amount;
-        if (balance <= 0) {
-            throw invalid_argument("Not sufficient funds for this withdrawal");
-        }
+    balance -= amount;
+    if (balance <= 0) {
+      throw invalid_argument("Not sufficient funds for this withdrawal");
     }
-    virtual double getBalance() {
-        return balance;
-    }
+  }
+  virtual double getBalance() { return balance; }
 };
 
 #endif
