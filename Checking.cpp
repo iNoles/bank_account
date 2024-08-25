9678beb8e506c9e9918ddbb45f8f99bb5f0f5406
Jonathan Steele <iNoles@users.noreply.github.com>
2024-08-24 23:01:07 -0400

    Update Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -1,5 +1,3 @@
-// Copyright 2024 Jonathan Steele
-
 #include "Checking.h"
 
 void CheckingAccount::displayAccountInfo() const {
@@ -8,15 +6,12 @@ void CheckingAccount::displayAccountInfo() const {
   cout << "Overdraft Limit: $" << overdraftLimit << "\n" << endl;
 }
 
-void CheckingAccount::makeWithdrawl(double amount) {
+void CheckingAccount::makeWithdrawal(double amount) {
   if (amount <= 0) {
-    throw invalid_argument("amount must be postive");
+    throw invalid_argument("Amount must be positive");
+  }
+  if (balance - amount < -overdraftLimit) {
+    throw invalid_argument("Withdrawal would exceed overdraft limit");
   }
   balance -= amount;
-  if (balance <= -overdraftLimit) {
-    throw invalid_argument("overdraft went over of limit occcured");
-  }
-  if (balance <= 0) {
-    throw invalid_argument("Not sufficient funds for this withdrawal");
-  }
 }
