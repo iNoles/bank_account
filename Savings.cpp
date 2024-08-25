84a391b3bf5b583379e115d0e3f6554d2aa6960d
Jonathan Steele <iNoles@users.noreply.github.com>
2024-08-24 23:02:04 -0400

    Update Savings.cpp

--- Savings.cpp
+++ Savings.cpp
@@ -1,10 +1,6 @@
-// Copyright 2024 Jonathan Steele
-
 #include "Savings.h"
 
 void SavingsAccount::displayAccountInfo() const {
-  using std::cout;
-  using std::endl;
   cout << "Savings Account Information" << endl;
   Account::displayAccountInfo();
   cout << "Interest Rate: " << interestRate << "% \n" << endl;
